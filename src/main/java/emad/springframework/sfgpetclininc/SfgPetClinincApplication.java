ef3a41c139a9692e6e5410f83e4169e613bda5be
emad-sfc <emad.sfc@gmail.com>
2021-11-01 19:26:33 +0530

    Initial commit

---
+++ src/main/java/emad/springframework/sfgpetclininc/SfgPetClinincApplication.java
@@ -0,0 +1,13 @@
+package emad.springframework.sfgpetclininc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinincApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetClinincApplication.class, args);
+	}
+
+}
